0ffe55f5774311a18b2be094cd885d6c29fc4b45
Vicente Aguirre <vicenteignacioac22@gmail.com>
2023-07-17 19:59:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+function App() {
+
+  return (
+    <div>
+      <h1 className='fs-1 fw-bold text-center'>Hello World</h1>
+      <Button variant="primary">Primary</Button>
+    </div>
+  )
+}
+
+export default App
