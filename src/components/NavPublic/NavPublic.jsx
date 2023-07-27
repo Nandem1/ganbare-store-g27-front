2501c07f97716f9320da38d8b28383c7805bfb95
Vicente Aguirre <vicenteignacioac22@gmail.com>
2023-07-26 20:42:19 -0400

    nav fix

--- src/components/NavPublic/NavPublic.jsx
+++ src/components/NavPublic/NavPublic.jsx
@@ -1,13 +1,14 @@
 import {RiLoginBoxFill} from 'react-icons/ri';
 import Nav from 'react-bootstrap/Nav';
 import './NavPublic.css'
+import {Link} from 'react-router-dom';
 
 const NavPublic = () => {
     return(
             <div className='p-3 mb-5 d-flex justify-content-center align-items-center'>
                 <Nav justify>
                     <Nav.Item>
-                        <Nav.Link href="/ganbare-store-g27-front/loginRegister" className='colorButton'><RiLoginBoxFill size={40} color='#8288c9'  />Iniciar sesión</Nav.Link>
+                        <Nav.Link as={Link} to="/ganbare-store-g27-front/loginRegister" className='colorButton'><RiLoginBoxFill size={40} color='#8288c9'  />Iniciar sesión</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </div>
