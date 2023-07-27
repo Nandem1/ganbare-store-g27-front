33ea9c9acbb4f3856a94f188ea84ebb629eec158
Vicente Aguirre <vicenteignacioac22@gmail.com>
2023-07-26 20:05:28 -0400

    arreglado v2 imagenes

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -25,16 +25,16 @@ function Home() {
         <div className='desktop descuentos-div-romboide p-3 me-5'>
           <div className='contenido-romboide d-flex align-content-center justify-content-between px-3'>
             <div className='d-flex align-items-center'>
-              <Image className='discount-image-romboide me-2' src='/src/assets/homepage/60percentdiscount.png' />
+              <Image className='discount-image-romboide me-2' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/60percentdiscount.png' />
               <h2>En juegos para Switch</h2>
             </div>
             <div className='d-flex'>
-              <Image className='switch-games-discount' src='/src/assets/homepage/supermarioparty.png' />
-              <Image className='switch-games-discount' src='/src/assets/homepage/pokemon.png' />
-              <Image className='switch-games-discount' src='/src/assets/homepage/mariokart.png' />
-              <Image className='switch-games-discount' src='/src/assets/homepage/animalcrossing.png' />
-              <Image className='switch-games-discount' src='/src/assets/homepage/zelda.png' />
-              <Image className='switch-games-discount' src='/src/assets/homepage/builders.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/supermarioparty.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/pokemon.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/mariokart.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/animalcrossing.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/zelda.png' />
+              <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/builders.png' />
             </div>
           </div>
         </div>
@@ -43,13 +43,13 @@ function Home() {
       {/* Descuentos div romboide Mobile*/}
       <div className='mobile descuentos-mobile-container p-2'>
         <div className='d-flex justify-content-evenly align-items-center mt-3'>
-          <Image className='discount-image-romboide' src='/src/assets/homepage/60percentdiscount.png' />
+          <Image className='discount-image-romboide' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/60percentdiscount.png' />
           <h2>En juegos para Switch</h2>
         </div>
         <div className='d-flex justify-content-center mt-3 mb-4'>
-          <Image className='switch-games-discount' src='/src/assets/homepage/mariokart.png' />
-          <Image className='switch-games-discount' src='/src/assets/homepage/animalcrossing.png' />
-          <Image className='switch-games-discount' src='/src/assets/homepage/zelda.png' />
+          <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/mariokart.png' />
+          <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/animalcrossing.png' />
+          <Image className='switch-games-discount' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/zelda.png' />
         </div>
       </div>
       {/* END OF Descuentos div romboide Mobile*/}
@@ -58,11 +58,11 @@ function Home() {
         <h2>Encuentra todo lo que buscas</h2>
       </div>
       <div className='desktop d-flex justify-content-evenly align-items-center mb-3'>
-        <div className='buscas-box-img desktop'><Image src='/src/assets/homepage/nintendoswitchdocked.png' /></div>
-        <div className='buscas-box-img desktop'><Image src='/src/assets/homepage/crysis2.png' /></div>
-        <div className='buscas-box-img desktop'><Image src='/src/assets/homepage/mangas.png' /></div>
-        <div className='buscas-box-img desktop'><Image src='/src/assets/homepage/kimetsufigura.png' /></div>
-        <div className='buscas-box-img desktop'><Image src='/src/assets/homepage/poleronotaku.png' /></div>
+        <div className='buscas-box-img desktop'><Image src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/nintendoswitchdocked.png' /></div>
+        <div className='buscas-box-img desktop'><Image src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/crysis2.png' /></div>
+        <div className='buscas-box-img desktop'><Image src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/mangas.png' /></div>
+        <div className='buscas-box-img desktop'><Image src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/kimetsufigura.png' /></div>
+        <div className='buscas-box-img desktop'><Image src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/poleronotaku.png' /></div>
       </div>
       {/* END OF Todo lo que buscas Desktop*/}
       {/* Todo lo que buscas Mobile */}
@@ -74,19 +74,19 @@ function Home() {
         <h2 className='text-center my-3 pt-3'>Nuestras marcas</h2>
         <Row lg={5} md={5} sm={1} xs={1}>
           <Col className='py-3'>
-            <Image className='marcas-img m-auto' src='/src/assets/homepage/nintendologo.png' />
+            <Image className='marcas-img m-auto' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/nintendologo.png' />
           </Col>
           <Col className='py-3'>
-            <Image className='marcas-img m-auto' src='/src/assets/homepage/funkologo.png' />
+            <Image className='marcas-img m-auto' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/funkologo.png' />
           </Col>
           <Col className='py-3'>
-            <Image className='marcas-img m-auto' src='/src/assets/homepage/marvellogo.png' />
+            <Image className='marcas-img m-auto' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/marvellogo.png' />
           </Col>
           <Col className='py-3'>
-            <Image className='marcas-img m-auto' src='/src/assets/homepage/dclogo.png' />
+            <Image className='marcas-img m-auto' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/dclogo.png' />
           </Col>
           <Col className='py-3'>
-            <Image className='marcas-img m-auto' src='/src/assets/homepage/pslogo.png' />
+            <Image className='marcas-img m-auto' src='https://raw.githubusercontent.com/Nandem1/ganbare-store-g27-front/main/src/assets/homepage/pslogo.png' />
           </Col>
         </Row>
       </Container>
