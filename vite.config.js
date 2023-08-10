77e0135e18e8cd74f6a80e44a54a3272e402dc3a
Vicente Aguirre <vicenteignacioac22@gmail.com>
2023-08-10 00:13:32 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ganbare-store-g27-front',
+  base: './',
   server: {
     port: 3500,
   },
